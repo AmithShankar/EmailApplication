90ea709d7815a87ebab39669587f35dfdbc147ec
Amith Shankar <71421438+AmithShankar@users.noreply.github.com>
2023-02-16 21:04:20 +0530

    Update Email.java

--- Email.java
+++ Email.java
@@ -9,20 +9,22 @@ public class Email {
 	private String password;
 	private String department;
 	private String email;
-	private int mailBoxCapacity = 500;
+	private int mailBoxCapacity;
 	private int passwordLength;
 	private String companySuffix = "ttg.com";
 	
+	Scanner scan = new Scanner(System.in);
+	
 	// Constructor
 	public Email(){
-		
+
 		// Call getFirstName and getLastName
 		this.firstName = setFirstName();
 		this.lastName = setLastName();
-		
+
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
-		
+
 		// Combine elements to generate email
 		if(department!=""){
 			this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + '@' + department + "." + companySuffix;
@@ -31,41 +33,38 @@ public class Email {
 			this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + '@' + department + companySuffix;
 		}
 		System.out.println("Your email is: " + this.email);
-		
+
 		// Alternate Email
 		changeEmail();
-		
+
 		// Call a method that returns a random password
 		this.password = randomPassword(passwordLength);
 		System.out.println("Your password is: " + this.password);
-		
+
 		// Change Password
 		changePassword();
-		
+
 		// Set mail box capacity
-		
+		setMailBoxCapacity();
 	}
 	
 	// Set firstName and lastName
-		public String setFirstName(){
-			System.out.println("Enter your First Name:");
-			Scanner scan = new Scanner(System.in);
-			String firstName = scan.next();
-			return firstName;
-		}
-		
-		public String setLastName(){
-			System.out.println("Enter your Last Name:");
-			Scanner scan = new Scanner(System.in);
-			String lastName = scan.next();
-			return lastName;
-		}
-		
+	public String setFirstName(){
+		System.out.println("Enter your First Name:");
+		String firstName = scan.next();
+		return firstName;
+	}
+
+	public String setLastName(){
+		System.out.println("Enter your Last Name:");
+		String lastName = scan.next();
+		return lastName;
+	}
+
 	// Ask for department 
 	private String setDepartment(){
-		System.out.println("New Worker: " + firstName + "\nDepartment Codes:\n1 for Sales\n2 for Development"
-				+ "\n3 for Accounting\n0 for none\nEnter department code: ");
-		Scanner scan = new Scanner(System.in);
+		System.out.println("New Worker: " + firstName + " " + lastName + "\nDepartment Codes:\n0. None\n1. Sales\n"
+				+ "2. Development\n3. Accounting\nEnter department code:");
 		int deptChoice = scan.nextInt();
 		if(deptChoice == 1){
 			return "sales";
@@ -80,11 +79,10 @@ public class Email {
 			return "";
 		}
 	}
-	
+
 	// Change email
 	private void changeEmail(){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Do you want to enter a alternate email? \n1 for Yes\n2 for No\nEnter your choice:");
+		System.out.println("Do you want to enter a alternate email?\n1. Yes\n2. No\nEnter your choice:");
 		while(true){
 			int choice = scan.nextInt();
 			if(choice == 1){
@@ -95,12 +93,12 @@ public class Email {
 					String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*"
 							+ "@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}";
 					Pattern pat = Pattern.compile(regex);
-			        if (pat.matcher(alt).matches()==true){
-			           setAlternateEmail(alt);
-			           System.out.println("Your email is: " + email);
-			           break;
-			        }
-			        System.out.println("The input you have entered is Invalid\nEnter valid alternate email");
+					if (pat.matcher(alt).matches()==true){
+						setAlternateEmail(alt);
+						System.out.println("Your email is: " + email);
+						break;
+					}
+					System.out.println("The input you have entered is Invalid\nEnter valid alternate email");
 				}
 				break;
 			}
@@ -109,14 +107,13 @@ public class Email {
 				break;
 			}
 			System.out.println("Invalid option");
-			System.out.println("Enter valid choice:\n1 for Yes\n2 for No");
+			System.out.println("Enter valid choice:\n1. Yes\n2. No");
 		}
 	}
-	
+
 	// Generate a random password
 	private String randomPassword(int length){
 		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%";
-		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Password Length(between 8 to 20):");
 		while(true){
 			length = scan.nextInt();
@@ -129,14 +126,13 @@ public class Email {
 				return new String(password); // Converts set of characters into String and returns
 			}
 			System.out.println("Invalid length detected\nEnter valid length:");
-			
+
 		}
 	}
-	
+
 	// Change Password
 	private void changePassword(){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Do you want to change password? \n1 for Yes\n2 for No\nEnter your choice:");
+		System.out.println("Do you want to change password?\n1. Yes\n2. No\nEnter your choice:");
 		while(true){
 			int choice = scan.nextInt();
 			if(choice == 1){
@@ -151,16 +147,16 @@ public class Email {
 				while(true){
 					String alt = scan.next();
 					String regex = "^(?=.*[0-9])"
-                       + "(?=.*[a-z])(?=.*[A-Z])"
-                       + "(?=.*[@#$%^&+=])"
-                       + "(?=\\S+$).{8,20}$";
+							+ "(?=.*[a-z])(?=.*[A-Z])"
+							+ "(?=.*[@#$%^&+=])"
+							+ "(?=\\S+$).{8,20}$";
 					Pattern pat = Pattern.compile(regex);
-			        if (pat.matcher(alt).matches()==true){
-			           setPassword(alt);
-			           System.out.println("Your password is: " + password);
-			           break;
-			        }
-			        System.out.println("The input you have entered is Invalid\nEnter valid password:");
+					if (pat.matcher(alt).matches()==true){
+						setPassword(alt);
+						System.out.println("Your password is: " + password);
+						break;
+					}
+					System.out.println("The input you have entered is Invalid\nEnter valid password:");
 				}
 				break;
 			}
@@ -169,55 +165,59 @@ public class Email {
 				break;
 			}
 			System.out.println("Invalid option");
-			System.out.println("Enter valid choice:\n1 for Yes\n2 for No");
+			System.out.println("Enter valid choice:\n1. Yes\n2. No");
 		}
 	}
-	
+
 	// Set mail capacity
-	public void setMailBoxCapacity(int capacity){
-		this.mailBoxCapacity = capacity;
-		
+	public void setMailBoxCapacity(){
+		if(department == "sales"){
+			this.mailBoxCapacity = 200;
+		}
+		else if(department == "dev"){
+			this.mailBoxCapacity = 300;
+		}
+		else if(department == "acc"){
+			this.mailBoxCapacity = 400;
+		}
+		else{
+			this.mailBoxCapacity = 500;
+		}
+
 	}
-	
+
 	// Set alternative email
 	public void setAlternateEmail(String altEmail){
 		this.email = altEmail;
 	}
-	
+
 	// set password
 	public void setPassword(String password){
 		this.password = password;
 	}
-	
+
 	// Get methods
 	public String getFirstName(){
 		return firstName;
 	}
-	
+
 	public String getLastName(){
 		return lastName;
 	}
-	
+
 	public String getPassword(){
 		return password;
 	}
-	
+
 	public String getDepartment(){
 		return department;
 	}
-	
+
 	public int getMailBoxCapacity(){
 		return mailBoxCapacity;
 	}
-		
+
 	public String getEmail(){
 		return email;
-	}
-	
-	// Display
-	public String showInfo(){
-		return "DISPLAY NAME: " + firstName + " " + lastName + 
-			"\nCOMPANY EMAIL: " + email +
-			"\nMAILBOX CAPACITY: " + mailBoxCapacity + " mb";
 	}
 }
